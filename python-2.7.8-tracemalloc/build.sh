05c3d11b09de78a1dfa4e909699a68a544c4ed52
immerrr <immerrr@gmail.com>
2014-12-01 16:55:39 +0300

    Add backport recipes for pytracemalloc
    - pytracemalloc: library itself
    - python-2.7.8-tracemalloc: python with pep445 backport patch
    - enable-tracemalloc: metapackage providing "pytracemalloc" feature

---
+++ python-2.7.8-tracemalloc/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+./configure --prefix=$PREFIX \
+    --enable-shared --enable-ipv6 --enable-unicode=ucs4 \
+    CPPFLAGS="-I$PREFIX/include" \
+    LDFLAGS="-L$PREFIX/lib -Wl,-rpath=$PREFIX/lib" \
+    && make -j$(getconf _NPROCESSORS_ONLN) \
+    && make install
