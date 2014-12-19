0dec837f848b1816cd84f4b04e69451b35ad3c9b
immerrr <immerrr@gmail.com>
2014-12-19 16:52:17 +0300

    ncurses: try using global terminfo databases by default
    rxvt-unicode-256color terminfo is for example distributed with rxvt and
    is usually provided by host systems in extra package.

--- ncurses/build.sh
+++ ncurses/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 sh ./configure --prefix=$PREFIX \
-    --with-shared --enable-overwrite \
+    --with-shared --enable-overwrite --disable-termcap \
     --without-debug --without-ada --without-manpages \
+    --with-terminfo-dirs=/etc/terminfo:/lib/terminfo:/usr/share/terminfo:$PREFIX/share/terminfo \
+    --with-default-terminfo-dir=/usr/share/terminfo \
     --with-termlib=tinfo --enable-widec
 
 make -j$(getconf _NPROCESSORS_ONLN)
